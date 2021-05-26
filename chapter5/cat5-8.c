2794ebd1debdc65493b40cf5a3bdab7f0db43edd
kokoakuma <aikw9101@gmail.com>
2021-05-26 23:07:29 +0900

    :sparkles: finish c5-8

---
+++ chapter5/cat5-8.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+static void do_cat(FILE *f);
+
+int
+main(int argc, char *argv[])
+{
+  int i;
+  // argv[0] = file name, argv < 2はファイル名しかなく、引数が指定されていない状態になる
+  if (argc == 1)
+  {
+    do_cat(stdin);
+  }
+  else
+  {
+    for (i = 1; i < argc; i++)
+    {
+      FILE *f;
+      f = fopen(argv[i], "r");
+      if (!f)
+      {
+        perror(argv[i]);
+        exit(1);
+      }
+      do_cat(f);
+      fclose(f);
+    }
+  exit(0);
+  }
+}
+
+static void do_cat(FILE *f)
+{
+  int c;
+
+  while ((c = fgetc(f)) != EOF)
+  {
+    if (putchar(c) < 0) exit(1);
+  }
+}
